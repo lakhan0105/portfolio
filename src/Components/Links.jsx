7e80bdde3d18711d0781ca5dd590ba0ac710c9dc
lakhan0105 <lakhan0105@gmail.com>
2024-11-11 00:54:52 +0530

    changed the navbar ui

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -9,11 +9,11 @@ const data = [
 
 function Links() {
   return (
-    <ul className="flex gap-5">
+    <ul className="flex gap-7">
       {data.map((item) => {
         const { linkName } = item;
         return (
-          <li className="capitalize cursor-pointer text-md hover:text-lime-200/[0.9]">
+          <li className="capitalize cursor-pointer text-lg hover:text-lime-200/[0.9]">
             <Link to={linkName} smooth duration={500}>
               {linkName}
             </Link>
